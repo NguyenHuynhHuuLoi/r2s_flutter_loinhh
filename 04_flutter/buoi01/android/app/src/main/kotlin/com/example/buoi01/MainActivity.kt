55a396d23c85f4230c97f74b903bdc1f54769feb
NguyenHuynhHuuLoi <105725988+NguyenHuynhHuuLoi@users.noreply.github.com>
2025-03-31 21:38:20 +0700

    Delete 04_flutter directory

--- 04_flutter/buoi01/android/app/src/main/kotlin/com/example/buoi01/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.buoi01
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- 04_flutter/buoi02/android/app/src/main/kotlin/com/example/buoi02/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.buoi02
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
